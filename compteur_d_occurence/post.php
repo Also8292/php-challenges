93377364bdb7fb4d60539928b28633d3eed70db0
Aliou-Sow <sowalils1992@gmail.com>
2018-11-29 10:57:38 +0000

    compteur d'occurence

---
+++ compteur_d_occurence/post.php
@@ -0,0 +1,16 @@
+<?php
+
+    if(isset($_GET['occurence']) && isset($_GET['text'])) {
+        $mot = $_GET['occurence'];
+        $text = $_GET['text'];
+
+        if($mot != "" && $text != "") {
+            echo 'Le mot <strong>' . $mot . '</strong> apparaît ' . substr_count($text, $mot) . ' fois dans ce texte';
+        }
+
+        else {
+            echo 'Veuillez entrer un texte et le l\'occurence à verifier';
+        }
+    }
+    
+?>
